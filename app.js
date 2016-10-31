060eb1b8655c1b861c9f997397ae1eac1c7189f5
Jeckson <cjswp122@gmail.com>
2016-10-31 13:21:51 +0900

    my node.js second step
    - create very simple server

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("hello world!!");
+console.log(">>>>> app.js start");
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type' : 'text/plain' });
+    res.end('server is started');
+});
+
+server.listen(8000);
+console.log("app.js done <<<<<");
