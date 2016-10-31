21ca4379dbc8a119f2018852d130a6e36ff20f6b
Jeckson <cjswp122@gmail.com>
2016-10-31 14:00:05 +0900

    my node.js third step
    - create very simple server with using express

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 console.log(">>>>> app.js start");
-var http = require('http');
+var express = require('express')
+    , http  = require('http')
+    , app   = express()
+    , server= http.createServer(app);
 
-var server = http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type' : 'text/plain' });
-    res.end('server is started');
+app.get('/', function(req, res){
+    res.send('Hello /');
 });
 
-server.listen(8000);
+app.get('/world.html', function(req, res){
+    res.send('HEY!');
+});
+
+server.listen(8000, function () {
+    console.log("Express server listening on port "+ server.address().port);
+});
 console.log("app.js done <<<<<");
